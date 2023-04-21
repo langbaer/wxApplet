e5eb7ea535075ec0024033021dc70f968408b6ff
langbaer <1270076513@qq.com>
2023-04-21 21:45:18 +0800

    完成了对首页的开发

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 // app.js
+//////引入@escook/request-miniprogram第三方包发起https请求
+import { $http } from '@escook/request-miniprogram'
+wx.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+/////请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  wx.showLoading({
+    title: '数据加载中...',
+  })
+},
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  wx.hideLoading()
+}
+
+/////挂载异步promise
+import {promisifyAll} from 'miniprogram-api-promise'
+const wxp = wx.p ={}
+promisifyAll(wx,wxp)
 App({
+ 
   onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
@@ -12,6 +33,15 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+
+    //请求数据失败的函数
+    wx.$showMsg = function (title = '请检查你的网络设置！', duration = 2000) {
+      wx.showToast({
+        title,
+        duration,
+        icon:'error'
+      })
+    }
   },
   globalData: {
     userInfo: null
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,86 @@
 // pages/home/home.js
+import {createStoreBindings} from 'mobx-miniprogram-bindings'
+import {store} from '../../store/store' ////引入store
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    spwierList:[],
+    navList:[],
+    floorList:[],
+    marginData:'10rpx',
+    marginData2:0,
+    floorNavUrl:''
+  },
 
+  /////发起轮播图数据请求的函数
+  async getSpwierList(){
+    const {data:res} = await wx.$http.get('/api/public/v1/home/swiperdata')
+    if(res.meta.status!==200){
+      return wx.$showMsg()
+    }
+    this.setData({
+      spwierList:res.message
+    })
+  },
+
+  /////发起获取分类导航栏数据请求的函数
+  async getNavList(){
+    const {data:res} = await wx.$http.get('/api/public/v1/home/catitems')
+    if(res.meta.status!==200){
+      return wx.$showMsg()
+    }
+    this.setData({
+      navList:res.message
+    })
+  },
+
+  /////发起楼层数据请求的函数
+  async getFloorList(){
+    const {data:res} = await wx.$http.get('/api/public/v1/home/floordata')
+    if(res.meta.status!==200){return wx.$showMsg()}
+    res.message.forEach(frist=>{
+      frist.product_list.forEach(secound=>{
+        secound.url='/subpkg/goods_list/good_list'+'?'+secound.navigator_url.split('?')[1]
+        secound.navigator_url
+      })
+    })
+    this.setData({
+      floorList:res.message
+    })
+    console.log(this.data.floorList)
+  },
+
+  /////分类导航的点击跳转函数   bindtap
+  navToPages(e){
+    if(e.target.dataset.name === '分类'){
+      ///点击跳转页面
+      wx.switchTab({
+        url: '/pages/cate/cate',
+      })
+      ///点击跳转tabbar
+      this.updateTabbarStatus(2)
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    /////发起轮播图的数据请求
+    this.getSpwierList()
+    /////发起分类导航栏的数据请求
+    this.getNavList()
+    //////发起楼层数据请求
+    this.getFloorList()
+    /////请求store的数据
+    this.storeBindings = createStoreBindings(this,{
+      store,
+      fields:[],
+      actions:['updateTabbarStatus']
+    })
   },
 
   /**
@@ -40,6 +108,8 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
+    ////卸载store的数据
+    this.storeBindings.desroyStoreBindings()
 
   },
 
